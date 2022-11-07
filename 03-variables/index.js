6090cb09392d66fab1250b3da3b2a93d52d5867e
Vladislav <90001844+vladargus@users.noreply.github.com>
2022-11-07 13:22:08 +0300

    Add files via upload

---
+++ 03-variables/index.js
@@ -0,0 +1,16 @@
+// JavaScript-код с созданием переменных
+
+/*Автор:
+Владислав Ямалиев*/
+
+const myName = 'Владислав'
+const mySurname = 'Ямалиев'
+let myFavoriteDrink = 'Кофе'
+let myFavoriteAnimal = 'Кот'
+const myFavoriteProgrammingLanguage = 'JavaScript' // JS - one love
+
+console.log('Моё имя:', myName)
+console.log('Моя фамилия:', mySurname)
+console.log('Мой любимый напиток:', myFavoriteDrink)
+console.log('Моё любимое домашнее животное:', myFavoriteAnimal)
+console.log('Мой любимый язык программирования:', myFavoriteProgrammingLanguage)
---
+++ 04-cars/cars.js
@@ -0,0 +1,22 @@
+const carName = 'Lamborghini Huracán'
+let maxCarSpeed = '310 км/ч'
+let carOwner = 'Владислав Ямалиев'
+
+alert(`Марка автомобиля: ${carName}`)
+alert(`Максимальная скорость: ${maxCarSpeed}`)
+alert(`Владелец: ${carOwner}`)
+
+maxCarSpeed = '315 км/ч'
+carOwner = 'Александр Бессмертнов'
+console.log('У машины изменилась скорость:', maxCarSpeed)
+console.log('У машины изменился владелец:', carOwner)
+
+maxCarSpeed = '320 км/ч'
+carOwner = 'Максим Филанович'
+console.log('У машины изменилась скорость:', maxCarSpeed)
+console.log('У машины изменился владелец:', carOwner)
+
+maxCarSpeed = '325 км/ч'
+carOwner = 'Владилен Минин'
+console.log('У машины изменилась скорость:', maxCarSpeed)
+console.log('У машины изменился владелец:', carOwner)
