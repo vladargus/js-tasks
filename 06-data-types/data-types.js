aeb7edd55faeb2e1856c1f7f023c709433bcb70b
Vladislav <90001844+vladargus@users.noreply.github.com>
2022-11-07 17:40:46 +0300

    Add files via upload

---
+++ 06-data-types/data-types.js
@@ -0,0 +1,23 @@
+const myName = 'Vlad'
+const age = 29
+const isMarried = true
+const job = null
+const currentLocation = undefined
+const favorites = {
+  game: 'Skyrim',
+  music: 'RetroWave',
+  movie: 'Ready Player One',
+  car: 'Lamborghini Huracán',
+  number: 21,
+}
+const id = Symbol('id')
+const bigNumber = 21n
+
+console.log('myName', myName, typeof myName)
+console.log('age', age, typeof age)
+console.log('isMarried', isMarried, typeof isMarried)
+console.log('job', job, typeof job)
+console.log('currentLocation', currentLocation, typeof currentLocation)
+console.log('favorites', favorites, typeof favorites)
+console.log('id', id, typeof id)
+console.log('bigNumber', bigNumber, typeof bigNumber)
---
+++ 07-dynamic-typing/index.js
@@ -0,0 +1,11 @@
+let num = 21
+num = 'string'
+alert(num)
+
+let str = 'string'
+str = 21
+alert(str)
+
+let bool = true
+bool = null
+alert(bool)
